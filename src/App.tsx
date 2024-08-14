fb40b24fbe37930a003e5acc21aa8934e57c0ec6
Nino Khurtsidze <103818368+Ninunutsi@users.noreply.github.com>
2024-08-15 02:00:30 +0400

    feat/installing framer motion, adding simple animation to first card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Form from './components/Form'
 import Header from './components/Header'
+import Introduction from './components/Introduction'
 
 function App() {
   return (
     <div className="bg-[#fcfdff] w-full bg-[url('./assets/wave-haikei.svg')] bg-cover bg-center h-screen">
       <Header />
-      <Form />
+      {/* <Form /> */}
+      <Introduction />
     </div>
   )
 }
---
+++ src/components/Animation/Animation.tsx
@@ -0,0 +1,25 @@
+import { motion } from "framer-motion";
+import React from "react";
+
+interface AnimatedWrapperProps {
+  children: React.ReactNode;
+  animationKey: string;
+}
+
+const AnimatedWrapper: React.FC<AnimatedWrapperProps> = ({
+  children,
+  animationKey,
+}) => {
+  return (
+    <motion.div
+      key={animationKey}
+      initial={{ y: -30, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default AnimatedWrapper;
