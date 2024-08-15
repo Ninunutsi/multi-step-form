72c45ee7cd67cfb5609ea85cb05c623173f34aa1
Nino Khurtsidze <103818368+Ninunutsi@users.noreply.github.com>
2024-08-15 09:06:16 +0400

    fix/store.ts

--- src/store.ts
+++ src/store.ts
@@ -1,49 +1,13 @@
-import { create } from "zustand";
-
-type FormFieldsI = {
-  username: string;
-  password: string;
-  email: string;
-};
+import { create } from 'zustand'
 
 type FormType = {
-  step: number;
-  setStep: (step: number) => void;
-  handleNext: (
-    trigger: (name: string) => Promise<boolean>,
-    name: string
-  ) => Promise<void>;
-  handlePrev: () => void;
-  handleSubmitForm: (data: FormFieldsI) => Promise<void>;
-};
+  step: number
+  setStep: (step: number) => void
+}
 
 export const useFormStore = create<FormType>((set) => ({
   step: 0,
   setStep: (newStep: number) => {
-    set({ step: newStep });
+    set({ step: newStep })
   },
-  handleNext: async (trigger, name) => {
-    const isValid = await trigger(name);
-    set((state) => {
-      const newStep = isValid
-        ? state.step < 3
-          ? state.step + 1
-          : 4
-        : state.step;
-      return { step: newStep };
-    });
-  },
-  handlePrev: () => {
-    set((state) => ({
-      step: state.step > 1 ? state.step - 1 : state.step,
-    }));
-  },
-  handleSubmitForm: async (data: FormFieldsI) => {
-    try {
-      await new Promise((resolve) => setTimeout(resolve, 300));
-      console.log("Form data submitted successfully:", data);
-    } catch (error) {
-      console.error("Error submitting form:", error);
-    }
-  },
-}));
+}))
